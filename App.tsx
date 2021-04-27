0e8032df732a3c5edc51c376bad501d32bdbd197
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2021-04-27 08:39:00 +0800

    Fixed ui kitten installation problem, update app.tsx to use ui kitten and eva design

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
+
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +17,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ApplicationProvider>
     );
   }
 }
