dbfd5632a524e5e18e921799f6a4e9de674eb3f3
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2021-05-03 15:25:54 +0800

    Add icons.tsx

---
+++ components/Icons.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+
+import { Icon } from '@ui-kitten/components';
+
+
+export default {
+  Book: (props: any) => <Icon name='book-outline' {...props} />,
+  Setting: (props: any) => <Icon name='settings-outline' {...props} />,
+  Pen: (props: any) => <Icon name='edit-outline' {...props} />,
+}
