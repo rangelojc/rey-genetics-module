796407394372a4dae4925f6000d222663cc7e903
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2021-05-05 12:29:07 +0800

    Added dummy content for lessons

--- assets/lessons/lesson3.tsx
+++ assets/lessons/lesson3.tsx
@@ -1,24 +1,36 @@
-import React, {Component} from "react";
-import {View, Text} from "react-native"
+import React, { Component } from "react";
+import { View, Text } from "react-native"
 
 const styles = {
-    container: {
+  container: {
 
-    },
-    title: {
+  },
+  title: {
 
-    }
-}
-export default class Lesson3 extends Component{
-    constructor(props: any){
-      super(props);
-    }
-   
-    public render(){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.title}>Lesson 3 content</Text>
-        </View>
-      );
-    }
   }
+}
+export default class Lesson3 extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Lesson 3 content</Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </View>
+    );
+  }
+}
--- assets/lessons/lesson4.tsx
+++ assets/lessons/lesson4.tsx
@@ -1,24 +1,49 @@
-import React, {Component} from "react";
-import {View, Text} from "react-native"
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
 
 const styles = {
-    container: {
+  container: {
 
-    },
-    title: {
+  },
+  title: {
 
-    }
-}
-export default class Lesson4 extends Component{
-    constructor(props: any){
-      super(props);
-    }
-   
-    public render(){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.title}>Lesson 4 content</Text>
-        </View>
-      );
-    }
   }
+}
+export default class Lesson4 extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Lesson 4 content</Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </View>
+    );
+  }
+}
