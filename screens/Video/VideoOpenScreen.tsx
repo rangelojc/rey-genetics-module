1eb6d456f30f4cc097653d51d0f245ac9cdfad6e
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2021-09-29 18:45:47 +0800

    Add expo-asset dependency, implement video loading

--- screens/Video/VideoOpenScreen.tsx
+++ screens/Video/VideoOpenScreen.tsx
@@ -1,72 +1,94 @@
 import React, { useRef, useState } from 'react';
-import { Video, AVPlaybackStatus } from 'expo-av';
+import { Asset, useAssets } from 'expo-asset';
 
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
+
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 import GlobalStyles from "../../styles/GlobalStyles";
 import Header from "../../components/Header";
+import { FlexViewCrossStyleProps } from '@ui-kitten/components/devsupport';
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    alignItems: 'flex-start',
+    backgroundColor: '#000',
   },
   video: {
     alignSelf: 'center',
-    width: 320,
+    width: "100%",
     height: 360,
+    backgroundColor: '#000',
   },
   buttons: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  notFound: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
 
-export default function VideoOpenScreen({ route, navigation }: any) {
+const VideoComponent = (props: any) => {
   const video = useRef(null);
-  const [status, setStatus] = useState({ isPlaying: "Pause" });
+  const [status, setStatus] = useState();
+
+  return (
+    <Video
+      ref={video}
+      style={styles.video}
+      source={props.video}
+      useNativeControls
+      resizeMode="contain"
+      isLooping
+    //onPlaybackStatusUpdate={status => setStatus(status)}
+    />
+  )
+}
+
+const NoVideoFound = (props: any) => {
+  return (
+    <View style={styles.notFound}>
+      <Text>Video not found</Text>
+    </View>
+  )
+}
+
+export default function VideoOpenScreen({ route, navigation }: any) {
+  const [videoFiles] = useAssets(
+    [
+      require('../../assets/videos/video1.mp4'),
+      require('../../assets/videos/video2.mp4')
+    ]
+  );
 
   let videoTitle = '';
   let videoFileName = '';
-  let loadedVideo = undefined;
+  let videoFile = {};
 
   if (useIsFocused()) {
     videoTitle = route.params.params.videoTitle;
     videoFileName = route.params.params.videoFileName;
-
-    switch (videoFileName) {
-      case "video1": require(`../../assets/videos/video1.mp4`); break;
-    }
+    videoFile = videoFiles?.find(v => v.name === videoFileName) || {};
   }
 
-  console.log(loadedVideo);
+  console.log(videoFile, videoFileName)
 
   return (
     <View style={GlobalStyles.mainContainer}>
       <Header title={videoTitle} backAction="true" navigation={navigation} />
 
       <View style={{ ...GlobalStyles.column, ...GlobalStyles.center, ...GlobalStyles.flex }}>
-        <Video
-          ref={video}
-          style={styles.video}
-          source={loadedVideo}
-          useNativeControls
-          resizeMode="contain"
-          isLooping
-        //onPlaybackStatusUpdate={status => setStatus(() => status)}
-        />
-        {/* <View style={styles.buttons}>
-          <Button
-            title={status.isPlaying ? 'Pause' : 'Play'}
-            onPress={() =>
-              status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-            }
-          />
-        </View> */}
+        {
+          Object.keys(videoFile).length ? <VideoComponent video={videoFile} /> : <NoVideoFound />
+        }
       </View>
     </View>
   );
