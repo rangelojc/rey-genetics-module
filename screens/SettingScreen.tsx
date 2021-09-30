26777c18b1c763f4371212bed6cc6e7554842f88
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2021-09-30 16:20:14 +0800

    Transform settings screen to functional component

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -7,91 +7,7 @@ import { Text, Input, Button } from '@ui-kitten/components';
 import AsyncStorage from "../helpers/AsyncStorage"
 import GlobalStyles from "../styles/GlobalStyles";
 import Header from "../components/Header";
-export default class SettingScreen extends React.Component {
-  state: any;
 
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      formInput: {
-        firstName: '',
-        lastName: ''
-      },
-      applyButtonState: 'noChange', //noChange, hasChange, saved
-      firstName: '',
-      lastName: ''
-    }
-  }
-
-  async initState() {
-    let firstName = await AsyncStorage.get('rgm_firstName', 'User');
-    let lastName = await AsyncStorage.get('rgm_lastName', 'User');
-    this.setState({ firstName, lastName });
-  }
-
-  async setFormInput(key: string, val: string) {
-    this.setState({
-      formInput: {
-        ...this.state.formInput,
-        [key]: val,
-      }
-    });
-
-    this.setState({ applyButtonState: 'hasChange' })
-  }
-
-  async applySettings() {
-    for (var key in this.state.formInput) {
-      this.setState({ [key]: this.state.formInput[key] })
-      await AsyncStorage.set('rgm_' + key, this.state.formInput[key]);
-    }
-
-    this.setState({ applyButtonState: 'saved' });
-  }
-
-  async componentDidMount() {
-    await this.initState();
-  }
-
-  public render() {
-    let applyButton = null;
-
-    switch (this.state.applyButtonState) {
-      case "noChange": applyButton = <Button disabled={true} onPress={() => this.applySettings()}>Apply</Button>; break;
-      case "hasChange": applyButton = <Button disabled={false} onPress={() => this.applySettings()}>Apply</Button>; break;
-      case "saved": applyButton = <Button disabled={true} onPress={() => this.applySettings()}>Applied</Button>; break;
-    }
-
-    return (
-      <View style={GlobalStyles.mainContainer}>
-        <Header title="Settings" />
-
-        <View style={{ ...GlobalStyles.containerWrapper }}>
-
-          <Text>First Name</Text>
-          <Input
-            placeholder='First Name'
-            defaultValue={this.state.firstName}
-            style={styles.inputBox}
-            onChangeText={val => this.setFormInput('firstName', val)}
-          />
-
-          <Text>Last Name</Text>
-          <Input
-            placeholder='Last Name'
-            defaultValue={this.state.lastName}
-            style={styles.inputBox}
-            onChangeText={val => this.setFormInput('lastName', val)}
-          />
-
-          {applyButton}
-        </View>
-      </View>
-    )
-  }
-
-}
 
 let styles = StyleSheet.create({
   inputBox: {
@@ -102,3 +18,71 @@ let styles = StyleSheet.create({
     marginTop: 20
   }
 })
+
+export default () => {
+  const [defaultForm, setDefaultForm]: any = React.useState({});
+  const [firstName, setFirstName]: any = React.useState('');
+  const [lastName, setLastName]: any = React.useState('');
+
+  const [applyButtonState, setApplyButtonState] = React.useState('noChange'); //noChange, hasChange, saved
+
+  const initState = async function () {
+    let fn: any = await AsyncStorage.get('firstName', 'User');
+    let ln: any = await AsyncStorage.get('lastName', 'User');
+
+    setDefaultForm({
+      firstName: fn,
+      lastName: fn
+    });
+  }
+
+  const applySettings = async function () {
+    await AsyncStorage.set('firstName', firstName);
+    await AsyncStorage.set('lastName', lastName);
+    setApplyButtonState('saved');
+  }
+
+  const onInput = function (stateSetter: any, val: string) {
+    stateSetter(val);
+    setApplyButtonState('hasChange');
+  }
+
+  initState();
+
+  let applyButton = null;
+  switch (applyButtonState) {
+    case "noChange": applyButton =
+      <Button disabled={true} onPress={applySettings}>No Changes</Button>; break;
+    case "hasChange": applyButton =
+      <Button disabled={false} onPress={applySettings}>Apply</Button>; break;
+    case "saved": applyButton =
+      <Button disabled={true} onPress={applySettings}>Applied</Button>; break;
+  }
+
+  return (
+    <View style={GlobalStyles.mainContainer}>
+      <Header title="Settings" />
+
+      <View style={{ ...GlobalStyles.containerWrapper }}>
+
+        <Text>First Name</Text>
+        <Input
+          placeholder='First Name'
+          defaultValue={defaultForm?.firstName}
+          style={styles.inputBox}
+          onChangeText={val => onInput(setFirstName, val)}
+        />
+
+        <Text>Last Name</Text>
+        <Input
+          placeholder='Last Name'
+          defaultValue={defaultForm?.lastName!}
+          style={styles.inputBox}
+          onChangeText={val => onInput(setLastName, val)}
+        />
+
+        {applyButton}
+      </View>
+    </View>
+  )
+}
